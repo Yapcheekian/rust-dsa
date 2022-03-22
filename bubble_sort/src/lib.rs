6e7a2744872eba10876bb9d041cb69a57b22ddc1
Little Yellow Dog <cheekian.yap@aiesec.net>
2022-03-22 08:46:11 +0800

    add bubble sort

--- bubble_sort/src/lib.rs
+++ bubble_sort/src/lib.rs
@@ -1,8 +1,25 @@
+pub fn bubble_sort<T: PartialOrd>(v: &mut [T]) {
+    for p in 0..v.len() - 1 {
+        let mut sorted = true;
+        for i in 0..(v.len() - 1) - p {
+            if v[i] > v[i + 1] {
+                v.swap(i, i + 1);
+                sorted = false;
+            }
+        }
+        if sorted {
+            return;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_bubble_sort() {
+        let mut v = vec![4, 6, 1, 8, 11, 13, 3];
+        bubble_sort(&mut v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
     }
 }
